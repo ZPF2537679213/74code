757c8abd71b2035b1e114b80bd6fbd6114364192
FeiGe <2537679213@qq.com>
2020-12-03 13:35:39 +0800

    第一个Java

--- java/src/HelloWord.java
+++
@@ -1,7 +0,0 @@
-import java.util.Scanner;
-public class HelloWord {//public:引用限制符；class：申明一个类；HelloWord：类名，注意用public class申明的类只能有一个，并且类名要和文件名相同
-    public static void main(String[]args){//（固定写法）static:静态；String[]args:形式参数
-        Scanner input=new Scanner(System.in);
-        System.out.println("HelloWorld");//输出
-    }
-}
---
+++ java1/src/HelloWorld.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class HelloWorld {//public:引用限制符；class：申明一个类；HelloWord：类名，注意用public class申明的类只能有一个，并且类名要和文件名相同
+    public static void main(String[]args) {//（固定写法）static:静态；String[]args:形式参数
+        Scanner input = new Scanner(System.in);
+        String str=input.next();
+        System.out.println(str);//输出
+    }
+}
