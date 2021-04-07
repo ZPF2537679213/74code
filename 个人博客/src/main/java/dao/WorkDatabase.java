583f9808911948a3098c63404f22848e79be3b7a
FeiGe <2537679213@qq.com>
2021-04-07 15:59:04 +0800

    个人博客

--- 个人博客/src/main/java/dao/WorkDatabase.java
+++ 个人博客/src/main/java/dao/WorkDatabase.java
@@ -57,4 +57,49 @@ public class WorkDatabase {
         JDBC.close(connection,statement,resultSet);
         return list;
     }
+    public int delete(int id) throws SQLException {
+        Connection connection=JDBC.dataSourse();
+        String sql="delete from articleinfo where id=?";
+        PreparedStatement preparedStatement=connection.prepareStatement(sql);
+        preparedStatement.setInt(1,id);
+        int result=preparedStatement.executeUpdate();
+        JDBC.close(connection,preparedStatement,null);
+        return result;
+    }
+    public MyArtical search(int id) throws SQLException {
+        Connection connection=JDBC.dataSourse();
+        String sql="select * from articleinfo where id=?";
+        PreparedStatement preparedStatement=connection.prepareStatement(sql);
+        preparedStatement.setInt(1,id);
+        ResultSet resultSet=preparedStatement.executeQuery();
+        MyArtical myArtical=new MyArtical();
+        if(resultSet.next()){
+            myArtical.setTitle(resultSet.getString("title"));
+            myArtical.setContent(resultSet.getString("content"));
+        }
+        JDBC.close(connection,preparedStatement,resultSet);
+        return myArtical;
+    }
+    public int updateArt(int id,String title,String content) throws SQLException {
+        Connection connection=JDBC.dataSourse();
+        String sql="update articleinfo set title=?,content=? where id=?";
+        PreparedStatement preparedStatement=connection.prepareStatement(sql);
+        preparedStatement.setString(1,title);
+        preparedStatement.setString(2,content);
+        preparedStatement.setInt(3,id);
+        int result=preparedStatement.executeUpdate();
+        JDBC.close(connection,preparedStatement,null);
+        return result;
+    }
+    public int addArt(int uid,String title,String content) throws SQLException {
+        Connection connection=JDBC.dataSourse();
+        String sql="insert into articleinfo (title,content,uid) values (?,?,?)";
+        PreparedStatement statement=connection.prepareStatement(sql);
+        statement.setString(1,title);
+        statement.setString(2,content);
+        statement.setInt(3,uid);
+        int result=statement.executeUpdate();
+        JDBC.close(connection,statement,null);
+        return result;
+    }
 }
